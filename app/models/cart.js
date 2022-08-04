bda0ab9ead1387bb7b68e354a6fe1028e471994f
Ryan May <94943821+mayday91@users.noreply.github.com>
2022-08-04 11:47:55 -0400

    working api

---
+++ app/models/cart.js
@@ -0,0 +1,30 @@
+// import dependencies
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
+
+const cartSchema = new Schema({
+  trackName: {
+    type: [String],
+    required: true
+  },
+  price: {
+    type: Number,
+    default: 1
+  },
+  user: {
+    type: String,
+    required: true
+  }
+},{
+  totalPrice: {
+    type: Number
+  },
+  orderActive: {
+    type: Boolean,
+    default: true,
+    required: true
+  }
+})
+
+
+module.exports = model('Cart', cartSchema)
