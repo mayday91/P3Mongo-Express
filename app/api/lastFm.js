0ba9092e40a1db72e2b59257db082753e471daf9
tinhi3 <lancetnguyen100@gmail.com>
2022-08-04 14:01:26 -0400

    fixed apikey and function structure

--- app/api/lastFm.js
+++
@@ -1,9 +0,0 @@
-
-
-export const getLastFmSong = () => {
-    return axios({
-        method: "GET",
-        url: "https://ws.audioscrobbler.com/2.0/?method=track.search&track=anthem&api_key=d8e8bce9ba4ee2c4e9813f3ccb16ed83&format=json&limit=30"
-    })
-}
-
